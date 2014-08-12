bd919249ad1ece02cea94a75430067baaca8f82f
mkfreeman <michael.k.freeman@gmail.com>
2014-08-12 10:14:32 -0700

    edited 2c, 2t

--- examples/wa_pop/assignment2.js
+++ examples/wa_pop/assignment2.js
@@ -19,15 +19,23 @@ populations = data.map(function(d) {return d.pop})
 // Sort the data variable by median age
 data.sort(function(a,b) {return a.age > b.age})
 
-// Based on this, what are the cities with the youngest/oldest median ages
+// Based on this, what are the cities with the youngest/oldest median ages?
 
-// Write a function that takes in an object (one element from the data array) and constructs the following sentence:
+// Write a function called "description" that takes in an object (one element from the data array) and constructs the following sentence:
 /*
 	"The median age in " {city} " is " {median age}
 
 */
 
+var description = function(city) {
+	return "The median age in " + city.city + " is " + city.pop
+}
+
 // Pass one of the data elements to your function and view the results
+description(data[10])
 
 // Using the array filter functionality, create an array called "maleCities" that has the names of cities where there are more men than women
+var maleCities  = data.filter(function(d) {return d.males>d.females})
 
+// How many of the top 100 cities have more males than females?
+maleCities.length
